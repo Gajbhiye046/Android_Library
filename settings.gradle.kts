b818fb9eb9d4df495f836cacb76ac92939c912c7
Abhishek Gajbhiye <abhishekkumar.gajbhiye@gmail.com>
2024-07-26 10:18:31 +0530

    Commmit from AS.

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "helloWorld"
+include(":app", ":flashlight")
+project(":flashlight").projectDir = File("C:\\Users\\ag928515.ttl\\AndroidStudioProjects\\How_to_convert_project_to_library\\flashlight\\app")
+
+ 
